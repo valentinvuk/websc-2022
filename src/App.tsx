4e367b839be98ba3d3ed0ed173b8a76cca1a77bc
Princiya Sequeira <princiya.marina@gmail.com>
2022-08-21 14:49:30 +0200

    add netlify helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Did you like this workshop?
         </p>
       </div>
       <p className="read-the-docs">
